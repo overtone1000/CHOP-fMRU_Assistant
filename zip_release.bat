dbd1d8d7bc8984f985a2c1e2bb13b98ab8447600
TylerRMoore@gmail.com <TylerRMoore@gmail.com>
2016-07-19 15:13:41 -0700

    Additional wiki image, changes to zip batch file.

--- zip_release.bat
+++ zip_release.bat
@@ -1,2 +1,8 @@
 set mydate=%date:~10,4%%date:~7,2%%date:~4,2%
-for /d %%a in (CHOP-fMRU_Assistant\bin\x64\Release) do ("C:\Program Files\7-zip\7z.exe" a "CHOP-fMRU_Assistant_Release.zip" "%%a\*")
+set zipname="CHOP-fMRU_Assistant_Release.zip"
+
+del %zipname%
+cd CHOP-fMRU_Assistant\bin\x64\Release
+for /d %%a in (.) do ("C:\Program Files\7-zip\7z.exe" a %zipname% "%%a\*")
+move %zipname% "..\..\..\.."
+cd "..\..\..\.."
